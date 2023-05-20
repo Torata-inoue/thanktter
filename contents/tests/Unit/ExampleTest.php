9a3adcbfb696ca857be88bb2334eeaac128eebbf
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-05-20 15:15:00 +0900

    phpunit OK！

--- contents/tests/Unit/ExampleTest.php
+++ contents/tests/Unit/ExampleTest.php
@@ -2,15 +2,29 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function testExample(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function testConnectDb(): void
+    {
+        $user = new User([
+            'name' => 'name',
+            'email' => 'email',
+            'password' => 'password'
+        ]);
+        $this->assertTrue($user->save());
     }
 }
