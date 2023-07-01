0ce8a8d408480d8e67dcd39682b779b251a3ef28
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-07-01 17:56:07 +0900

    loginベース作成

--- contents/routes/web.php
+++ contents/routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 $router->get('/', [\App\Http\Web\Controllers\Top\TopBaseController::class, 'index']);
+
+$router->get('/login', [\App\Http\Web\Controllers\Login\ShowLoginController::class, 'index']);
