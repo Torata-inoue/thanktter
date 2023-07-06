4a9e0150bfd8a372e469a8994bd142ab0f0a30c1
mfro-Inoue <torata.inoue@mfro.net>
2023-07-06 21:16:35 +0900

    認証/未認証のリダイレクト対応

--- contents/resources/ts/src/common/components/template/Template.tsx
+++ contents/resources/ts/src/common/components/template/Template.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { Header } from './header/Header';
+import { useAuthenticated } from '../../states/atoms/auth';
 
 type TemplateProps = { children: React.ReactNode };
-export const Template: React.FC<TemplateProps> = ({ children }) => (
-  <>
-    <Header />
-    <Container>{children}</Container>
-  </>
-);
+export const Template: React.FC<TemplateProps> = ({ children }) => {
+  const authenticated = useAuthenticated();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authenticated) {
+      navigate('/login');
+    }
+  }, [authenticated, navigate]);
+
+  if (!authenticated) {
+    return null; // Or return some kind of "Loading" indicator.
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>{children}</Container>
+    </>
+  );
+};
--- contents/resources/ts/src/common/states/atoms/auth.ts
+++ contents/resources/ts/src/common/states/atoms/auth.ts
@@ -1,16 +1,24 @@
 import { atom, useRecoilValue, useSetRecoilState } from 'recoil';
 import { useCallback } from 'react';
-import { findAuthApi } from '../../features/auth/authApi';
 import { globalRecoilKeys } from '../globalRecoilKeys';
 import { AuthType } from '../../constans/auth';
 
-const authState = atom<AuthType>({
+const authState = atom<AuthType | undefined>({
   key: globalRecoilKeys.USER,
-  default: findAuthApi(),
+  default: undefined,
 });
 
+type UseAuthenticated = () => boolean;
+export const useAuthenticated: UseAuthenticated = () => Boolean(useRecoilValue(authState));
+
 type UseAuthType = () => AuthType;
-export const useAuth: UseAuthType = () => useRecoilValue(authState);
+export const useAuth: UseAuthType = () => {
+  const auth = useRecoilValue(authState);
+  if (!auth) {
+    throw Error('ログインしてください');
+  }
+  return auth;
+};
 
 type UseSetAuthType = () => (auth: AuthType) => void;
 export const useSetAuth: UseSetAuthType = () => {
