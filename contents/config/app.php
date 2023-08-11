6ec2e065723b7e3f211743cc623da8b4e27632d1
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-11 16:26:52 +0900

    サービスプロバイダー登録

--- contents/config/app.php
+++ contents/config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // カスタムのサービスプロバイダー
+        App\Providers\ImageServiceProvider::class,
     ])->toArray(),
 
     /*
