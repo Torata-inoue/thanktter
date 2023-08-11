f899361539b4204eba5bb4cbb31f393ae14493d3
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-11 17:27:18 +0900

    AUth周り修正

--- contents/resources/ts/src/entries/app/features/login/login.ts
+++ contents/resources/ts/src/entries/app/features/login/login.ts
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import { api } from '../../../../common/utils/api';
 import { LoginFormType } from '../../hooks/form/useLoginForm';
 import { AuthType } from '../../../../common/constans/auth';
 
 type PostLoginApi = (data: LoginFormType) => Promise<{ user: AuthType }>;
-export const postLoginApi: PostLoginApi = (data) => api('/login', 'POST', data);
+export const postLoginApi: PostLoginApi = (data) =>
+  axios.get('/sanctum/csrf-cookie').then(() => api('/login', 'POST', data));
 
 type PostLogoutApiType = () => Promise<void>;
 export const postLogoutApi: PostLogoutApiType = () => api('/logout', 'POST');
