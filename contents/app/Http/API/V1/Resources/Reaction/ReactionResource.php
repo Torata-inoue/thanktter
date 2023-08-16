70e545e1ce804d400bc8ee6db5ec39014515227f
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-16 14:37:28 +0900

    resource修正

--- contents/app/Http/API/V1/Resources/Reaction/ReactionResource.php
+++ contents/app/Http/API/V1/Resources/Reaction/ReactionResource.php
@@ -2,25 +2,28 @@
 
 namespace App\Http\API\V1\Resources\Reaction;
 
+use App\Domains\Reaction\Reaction;
 use App\Domains\Reaction\ReactionType;
 use App\Http\API\V1\Resources\BaseResource;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 /**
- * @extends BaseResource<array<array{type: int, count: int}>>
+ * @extends BaseResource<Collection<int, Reaction>>
  */
 class ReactionResource extends BaseResource
 {
     /**
-     * @return array<string, int>
+     * @param Request $request
+     * @return array<ReactionType::name, int>
      */
     public function toArray(Request $request): array
     {
-        $reactions = [];
-        foreach ($this->resource as ['type' => $type, 'count' => $count]) {
-            $text = ReactionType::from($type)->getText();
-            $reactions[$text] = $count;
+        $reactions = $this->resource->keyBy('type');
+        $response = [];
+        foreach (ReactionType::cases() as $reactionType) {
+            $response[$reactionType->getLowerText()] = $reactions->get("{$reactionType->value}")?->count ?? 0;
         }
-        return $reactions;
+        return $response;
     }
 }
